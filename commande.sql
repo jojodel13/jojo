4f5e1eac04ef0318713d89a56a037bc8a25e380d
Avrillon Joel <avrillon.joelpro@gmail.com>
2023-10-20 15:56:17 +0200

    sql fini

--- commande.sql
+++ commande.sql
@@ -2,12 +2,12 @@ CREATE TABLE if not exists utilisateur CHARACTER SET = "utf8";
 
 CREATE TABLE clients (
     id INT UNSIGNED PRIMARY KEY auto_INCREMENT,
-    nom VARCHAR(70) NOT NULL,
-    prenom VARCHAR(70),
-    email VARCHAR(100),
+    Marque VARCHAR(70) NOT NULL,
+    vetement VARCHAR(70),
+    numero commande VARCHAR(100),
+    taille VARCHAR(10),
+    nom VARCHAR(70),
+    prenom VARCHAR(50),
     adresse VARCHAR(70),
-    telephone VARCHAR(20),
-    code_postal VARCHAR(10),
-    ville VARCHAR(70),
-    pays VARCHAR(70)
+    magasin VARCHAR(70)
 );
